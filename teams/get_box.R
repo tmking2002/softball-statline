bee8cb12b824f436e172ef8a01675fab4c51376f
Tyson King <tmking2002@gmail.com>
2023-09-15 12:10:24 -0400

    Update get_box.R

--- teams/get_box.R
+++ teams/get_box.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(softballR)
 
 adjust_hitting_box <- function(raw_box){
   
@@ -12,15 +13,15 @@ adjust_hitting_box <- function(raw_box){
   
 }
 
-box_2015 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2015.RDS") %>% adjust_hitting_box()
-box_2016 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2016.RDS") %>% adjust_hitting_box()
-box_2017 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2017.RDS") %>% adjust_hitting_box()
-box_2018 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2018.RDS") %>% adjust_hitting_box()
-box_2019 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2019.RDS") %>% adjust_hitting_box()
-box_2020 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2020.RDS") %>% adjust_hitting_box()
-box_2021 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2021.RDS") %>% adjust_hitting_box()
-box_2022 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2022.RDS") %>% adjust_hitting_box()
-box_2023 <- readRDS("~/Projects/softballR-data/data/d1_hitting_box_scores_2023.RDS") %>% adjust_hitting_box()
+box_2015 <- load_ncaa_softball_playerbox(season = 2015,category = "Hitting") %>% adjust_hitting_box()
+box_2016 <- load_ncaa_softball_playerbox(season = 2016,category = "Hitting") %>% adjust_hitting_box()
+box_2017 <- load_ncaa_softball_playerbox(season = 2017,category = "Hitting") %>% adjust_hitting_box()
+box_2018 <- load_ncaa_softball_playerbox(season = 2018,category = "Hitting") %>% adjust_hitting_box()
+box_2019 <- load_ncaa_softball_playerbox(season = 2019,category = "Hitting") %>% adjust_hitting_box()
+box_2020 <- load_ncaa_softball_playerbox(season = 2020,category = "Hitting") %>% adjust_hitting_box()
+box_2021 <- load_ncaa_softball_playerbox(season = 2021,category = "Hitting") %>% adjust_hitting_box()
+box_2022 <- load_ncaa_softball_playerbox(season = 2022,category = "Hitting") %>% adjust_hitting_box()
+box_2023 <- load_ncaa_softball_playerbox(season = 2023,category = "Hitting") %>% adjust_hitting_box()
 
 write.csv(box_2015, "~/Projects/softball-statline/teams/data/box_scores/d1_hitting_box_2015.csv")
 write.csv(box_2016, "~/Projects/softball-statline/teams/data/box_scores/d1_hitting_box_2016.csv")
