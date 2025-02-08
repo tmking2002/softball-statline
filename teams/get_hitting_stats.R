53908c1e0c8fa37085e28baa4ccdc7ea2e3f30fc
tmking2002 <tmking4@ncsu.edu>
2025-02-08 13:13:31 -0500

    fix hitting and pitching stats functions

--- teams/get_hitting_stats.R
+++ teams/get_hitting_stats.R
@@ -16,29 +16,33 @@ proper=function(x) paste0(toupper(substr(x, 1, 1)), tolower(substring(x, 2)))
 get_stats <- function(box, season){
   
   stats <- box %>% 
-    merge(teams, by.x = "team", by.y = "team_name") %>% 
-    separate(player, c("last", "first"), sep = ", ") %>% 
-    drop_na(first, last) %>% 
-    mutate(first =  proper(trimws(str_remove(first, "\\."))),
-           last = proper(trimws(str_remove(last, "\\."))),
-           player = paste(first, last),
-           season = .env$season,
-           across(5:28, \(col) ifelse(is.na(col), 0, col))) %>% 
-    filter(str_length(first) > 1,
-           str_length(last) > 1,
-           first != "Unknown") %>% 
-    merge(players, by = "player") %>% 
-    group_by(team_id, season, player, player_id) %>% 
-    summarise(across(5:25, sum),
-              avg = format(round(h / ab, 3), nsmall = 3),
-              obp = round((h + bb + hbp) / (ab + bb + hbp + sf + sh), 3),
-              slg = round(tb / ab, 3),
-              ops = format(round(obp + slg, 3), nsmall = 3)) %>% 
-    ungroup() %>% 
-    filter(ab > 0) %>% 
-    drop_na(avg, obp, ops) %>% 
-    select(team_id, player_id, season, player, ab, h, x2b, x3b, hr, rbi, r, bb, hbp, k, sb, cs, avg, obp, ops) %>% 
-    arrange(desc(ops))
+    merge(teams, by.x = "team", by.y = "team_name")
+  
+  if(season != 2025){
+    stats <- stats %>% 
+      separate(player, c("last", "first"), sep = ", ") %>% 
+      drop_na(first, last) %>% 
+      mutate(first =  proper(trimws(str_remove(first, "\\."))),
+             last = proper(trimws(str_remove(last, "\\."))),
+             player = paste(first, last))
+  } else{
+
+    stats <- stats %>% 
+      mutate(season = .env$season,
+             across(4:22, \(col) ifelse(is.na(col), 0, col))) %>% 
+      merge(players, by = "player") %>% 
+      group_by(team_id, season, player, player_id) %>% 
+      summarise(across(3:20, sum),
+                avg = format(round(h / ab, 3), nsmall = 3),
+                obp = round((h + bb + hbp) / (ab + bb + hbp + sf + sh), 3),
+                slg = round(tb / ab, 3),
+                ops = format(round(obp + slg, 3), nsmall = 3)) %>% 
+      ungroup() %>% 
+      filter(ab > 0) %>% 
+      drop_na(avg, obp, ops) %>% 
+      select(team_id, player_id, season, player, ab, h, x2b, x3b, hr, rbi, r, bb, hbp, k, sb, cs, avg, obp, ops) %>% 
+      arrange(desc(ops))
+  }
   
 }
 
--- teams/get_pitching_stats.R
+++ teams/get_pitching_stats.R
@@ -16,26 +16,35 @@ proper=function(x) paste0(toupper(substr(x, 1, 1)), tolower(substring(x, 2)))
 get_stats <- function(box, season){
   
   stats <- box %>% 
-    separate(player, c("last", "first"), sep = ", ") %>% 
-    merge(teams, by.x = "team", by.y = "team_name") %>% 
-    mutate(season = .env$season,
-           player = paste(first, last)) %>% 
-    filter(str_length(player) > 1) %>% 
-    mutate(lower_player = tolower(player)) %>% 
-    merge(players %>% mutate(lower_player = tolower(player)) %>% select(-player), by = "lower_player") %>% 
-    group_by(team_id, season, player, player_id) %>% 
-    summarise(across(c(er, ip, ha, bf, bb, hb, so, hr_a), 
-                     .fns = \(col) sum(as.numeric(col))),
-              era = format(round(er / ip * 7, 3), nsmall = 3),
-              opp_avg = round((ha) / (bf - bb - hb), 3),
-              whip = round((bb + hb + ha) / ip, 3),
-              fip = format(round((13 * hr_a + 3 * (bb + hb) - 2 * so) / ip + 2.4, 3), nsmall = 3),
-              ip = round(floor(ip) + ip %% 1 / 3, 1)) %>% 
-    ungroup() %>% 
-    filter(ip > 0) %>% 
-    drop_na(era, opp_avg, whip, fip) %>% 
-    select(team_id, player_id, season, player, ip, ha, bb, hb, so, hr_a, era, opp_avg, whip, fip) %>% 
-    arrange(desc(ip)) 
+    merge(teams, by.x = "team", by.y = "team_name")
+  
+  if(season != 2025){
+    stats <- stats %>% 
+      separate(player, c("last", "first"), sep = ", ") %>% 
+      drop_na(first, last) %>% 
+      mutate(first =  proper(trimws(str_remove(first, "\\."))),
+             last = proper(trimws(str_remove(last, "\\."))),
+             player = paste(first, last))
+  } else{
+  
+    stats <- stats %>% 
+      filter(str_length(player) > 1) %>% 
+      mutate(lower_player = tolower(player)) %>% 
+      merge(players %>% mutate(lower_player = tolower(player)) %>% select(-player), by = "lower_player") %>% 
+      group_by(team_id, season, player, player_id) %>% 
+      summarise(across(c(er, ip, ha, bf, bb, hb, so, hr_a), 
+                       .fns = \(col) sum(as.numeric(col))),
+                era = format(round(er / ip * 7, 3), nsmall = 3),
+                opp_avg = round((ha) / (bf - bb - hb), 3),
+                whip = round((bb + hb + ha) / ip, 3),
+                fip = format(round((13 * hr_a + 3 * (bb + hb) - 2 * so) / ip + 2.4, 3), nsmall = 3),
+                ip = round(floor(ip) + ip %% 1 / 3, 1)) %>% 
+      ungroup() %>% 
+      filter(ip > 0) %>% 
+      drop_na(era, opp_avg, whip, fip) %>% 
+      select(team_id, player_id, season, player, ip, ha, bb, hb, so, hr_a, era, opp_avg, whip, fip) %>% 
+      arrange(desc(ip)) 
+  }
   
   if(season == 2016){
     stats$hr_a <- 0
